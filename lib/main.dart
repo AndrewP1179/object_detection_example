83a2e5d571cf353480f02da480be5b9c2d42d795
Your Name <you@example.com>
2022-06-23 01:45:50 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'vision_detector_views/object_detector_view.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ObjectDetectorView(),
+    );
+  }
+}
